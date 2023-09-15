1fdec1a73f1bca87b5c9a0955f649d6beb80d680
AGARWAL Harsh Mohan 00072116 <hal5@nuodev25.dsone.3ds.com>
2023-09-15 12:19:30 -0400

    Uncommited changes remove script added

--- scripts/uncommitedChanges.sh
+++ scripts/uncommitedChanges.sh
@@ -5,6 +5,5 @@ GIT_STATUS="$(git status --porcelain)"
 VERSION="$(./release/relman.py --check-current --show-head-version)"
 if [ "$GIT_STATUS" != "" ]
 then
-    git add .
-    git commit -m "v${VERSION} commit"
+    git checkout -f -- "./stable/insights/Chart.lock"
 fi
